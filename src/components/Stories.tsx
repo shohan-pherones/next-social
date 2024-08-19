3f500ce5198de38f7ca7fc788cef776128ab9e79
shohan-pherones <shohan.sub.56@gmail.com>
2024-08-19 22:12:30 +0600

    stories

---
+++ src/components/Stories.tsx
@@ -0,0 +1,122 @@
+import Image from "next/image";
+
+const Stories = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs no-scrollbar">
+      <div className="flex gap-8 w-max">
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27663337/pexels-photo-27663337/free-photo-of-a-woman-in-a-pink-dress-walking-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Jennie</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
